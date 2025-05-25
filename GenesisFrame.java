ab5b5e21cfc9eebb151913a25dc2c9e8bbab8257
CRGaming117 <cantu7080@gmail.com>
2025-05-25 18:20:55 -0400

    Disabled frame resizing

--- GenesisFrame.java
+++ GenesisFrame.java
@@ -2,23 +2,29 @@ package genesis;
 
 import javax.swing.JFrame;
 
-public class GenesisFrame{
-	public static void main(String[] args) {
-		int w = 1400;
-		int h = 700;
-		JFrame frame = new JFrame("Genesis");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().add(new GenesisGraphics(w,h));
-		frame.pack();
-		frame.setVisible(true);
-	}
+public class GenesisFrame {
+  public static void main(String[] args) {
+    int w = 1400;
+    int h = 700;
+    JFrame frame = new JFrame("Genesis");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setResizable(false);
+    frame.getContentPane().add(new GenesisGraphics(w, h));
+    frame.pack();
+    frame.setVisible(true);
+  }
 }
 
-/*	DESCRIPTION
- *	
- *	Genesis is an application with the purpose of entertainment, give the player (alias the user) an ability to go through a maze and collect coins along their way.
- *	The game ends once the player finds the room where the diamon is and collects it. The program will keep running after winning until the application is closed.
+/*
+ * DESCRIPTION
+ * 
+ * Genesis is an application with the purpose of entertainment, give the player
+ * (alias the user) an ability to go through a maze and collect coins along
+ * their way.
+ * The game ends once the player finds the room where the diamon is and collects
+ * it. The program will keep running after winning until the application is
+ * closed.
  *
- *	Acknowledgements at the bottom of GenesisGraphics.java
- *	
+ * Acknowledgements at the bottom of GenesisGraphics.java
+ * 
  */
